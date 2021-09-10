bb79e48e348c3ac0085b70775a2c3108d98e5934
Alice Williams <alice@10degrees.uk>
2021-09-10 18:13:43 +0100

    Add icons.

--- app/cpt.php
+++ app/cpt.php
@@ -1,9 +1,19 @@
 <?php
 
 add_action('init', function() {
-	register_extended_post_type('speaker');
-	register_extended_post_type('event');
-	register_extended_post_type('favourite');
-	register_extended_post_type('review');
-	register_extended_post_type('timeline-event');
+	register_extended_post_type('speaker', [
+		'menu_icon' => 'dashicons-universal-access'
+	]);
+	register_extended_post_type('event', [
+		'menu_icon' => 'dashicons-calendar-alt'
+	]);
+	register_extended_post_type('favourite', [
+		'menu_icon' => 'dashicons-star-filled'
+	]);
+	register_extended_post_type('review', [
+		'menu_icon' => 'dashicons-welcome-write-blog'
+	]);
+	register_extended_post_type('timeline-event', [
+		'menu_icon' => 'dashicons-clock'
+	]);
 });
