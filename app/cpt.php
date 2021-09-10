bf0271000de394e86f56ba0056153d96db7d21fe
Alice Williams <alice@10degrees.uk>
2021-09-10 18:53:02 +0100

    Convert CPT file to OOP too.

--- app/cpt.php
+++ app/cpt.php
@@ -1,20 +1,30 @@
 <?php
 
-add_action('init', function() {
-	register_extended_post_type('speaker', [
-		'menu_icon' => 'dashicons-universal-access'
-	]);
-	register_extended_post_type('event', [
-		'menu_icon' => 'dashicons-calendar-alt'
-	]);
-	register_extended_post_type('favourite', [
-		'menu_icon' => 'dashicons-star-filled',
-		'has_archive' => false,
-	]);
-	register_extended_post_type('review', [
-		'menu_icon' => 'dashicons-welcome-write-blog'
-	]);
-	register_extended_post_type('timeline-event', [
-		'menu_icon' => 'dashicons-clock'
-	]);
-});
+class CPT {
+	public function __construct()
+	{
+		add_action('init', [$this, 'registerCPTs']);
+	}
+
+	public function registerCPTs()
+	{
+		register_extended_post_type('speaker', [
+			'menu_icon' => 'dashicons-universal-access'
+		]);
+		register_extended_post_type('event', [
+			'menu_icon' => 'dashicons-calendar-alt'
+		]);
+		register_extended_post_type('favourite', [
+			'menu_icon' => 'dashicons-star-filled',
+			'has_archive' => false,
+		]);
+		register_extended_post_type('review', [
+			'menu_icon' => 'dashicons-welcome-write-blog'
+		]);
+		register_extended_post_type('timeline-event', [
+			'menu_icon' => 'dashicons-clock'
+		]);
+	}
+}
+
+new CPT();
